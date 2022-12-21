6137234aa74efa3e0bdcbee954c74139b7b27e33
Oh Kyoung Ho <dhrudgh1061@gmail.com>
2022-12-21 20:05:18 +0900

    20221221

---
+++ aBasic/src/main/java/com/example/BoardController.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BoardController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	
+	@RequestMapping("/insertBoard")
+	public void insertBoard() {
+		logger.info("[ insertBoard ]");
+	}
+	
+	@RequestMapping("/getBoardList")
+	public void getBoardList() {
+		logger.info("[ getBoardList ]");
+		
+	}
+	
+	@RequestMapping("/getBoard")
+	public void getBoard() {
+		logger.info("[ getBoard ]");
+		
+	}
+
+}
