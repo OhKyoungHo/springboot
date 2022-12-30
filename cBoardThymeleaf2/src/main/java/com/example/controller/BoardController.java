b8fa04bf6ab0999ca4df51b500103f4656310186
Oh Kyoung Ho <dhrudgh1061@gmail.com>
2022-12-30 18:30:32 +0900

    20221230

---
+++ cBoardThymeleaf2/src/main/java/com/example/controller/BoardController.java
@@ -0,0 +1,65 @@
+package com.example.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.BoardVO;
+import com.example.service.BoardService;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+	
+	@Autowired
+	private BoardService boardService;
+	
+	@RequestMapping("/{step}")
+	public String viewPage(@PathVariable String step) {
+		return "board/"+ step;
+	}
+	
+	
+	@RequestMapping("/getBoardList")
+	public void getBoardList(Model m) {
+		BoardVO vo = new BoardVO();
+		List<BoardVO> list = boardService.getBoardList(vo);
+		System.out.println("[Controller:getBoardList 요청] 결과갯수 : " + list.size());
+		
+		m.addAttribute("boardList", list);
+	}
+	
+	// 글 등록
+	@RequestMapping("/saveBoard")
+	public String insertBoard(BoardVO vo) throws IOException {
+		boardService.insertBoard(vo);
+		return "redirect:/board/getBoardList";
+	}
+
+	// 글 수정
+	@RequestMapping("/updateBoard")
+	public String updateBoard(@ModelAttribute("board") BoardVO vo) {
+		boardService.updateBoard(vo);
+		return "redirect:/board/getBoard?seq="+vo.getSeq();
+	}
+
+	// 글 삭제
+	@RequestMapping("/deleteBoard")
+	public String deleteBoard(BoardVO vo) {
+		boardService.deleteBoard(vo);
+		return "redirect:/board/getBoardList";
+	}
+
+	// 글 상세 조회
+	@RequestMapping("/getBoard")
+	public void getBoard(BoardVO vo, Model model) {
+		model.addAttribute("board", boardService.getBoard(vo)); // Model 정보 저장			
+	}
+	
+}
